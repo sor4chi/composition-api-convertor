597ab0dab1ae662602ba73f7133b18e1451bdff4
kawamurasorachi <sorakawa0218@gmail.com>
2022-06-14 12:03:11 +0900

    Create Data Converting System

--- tests/converter/data.test.ts
+++ tests/converter/data.test.ts
@@ -3,11 +3,13 @@ import { ConvertedExpression } from '../../src/converter/types';
 import { convertTextToTypeScript } from '../../src/converter/utils/source';
 
 describe('convertDataExpression', () => {
-  it('should convert data expression', () => {
+  it('should convert primitive data expression', () => {
     const dataExpression = `
       data() {
         return {
-          foo: 'bar'
+          foo: 'bar',
+          hoge: 1,
+          fuga: true,
         }
       }
     `;
@@ -20,6 +22,8 @@ describe('convertDataExpression', () => {
 
     const collectConvertedDataExpression: ConvertedExpression[] = [
       { script: `const foo = ref('bar');` },
+      { script: `const hoge = ref(1);` },
+      { script: `const fuga = ref(true);` },
     ];
 
     expect(convertedDataExpression).toEqual(collectConvertedDataExpression);
